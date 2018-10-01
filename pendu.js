6919766423222d13bcbaee8b3afedc32be3423d7
taiQui <taiqui@localhost.localdomain>
2018-10-01 13:18:08 +0200

    fixing

--- pendu.js
+++ pendu.js
@@ -114,12 +114,6 @@ class Pendu {
           emb.addField("Pendu", this.devinemot + "\n\n" + this.currentLvl + "\n\n" + this.lettersaid, false);
           this.Pendu_gameMessage.edit(emb);
         } else if ((!this.mots.includes(msg.content)) && (!this.lettersaid.includes(msg.content))) {
-          if((!this.mots.includes(msg.content)) && (this.lettersaid.includes(msg.content))){
-            var emb = new discord.RichEmbed();
-            emb.addField("Pendu", this.devinemot + "\n\n" + this.currentLvl + "\n\n" + this.lettersaid + "\n\n " + "You already said this letter ! : " + letter, false);
-            this.Pendu_gameMessage.edit(emb);
-            return;
-          }
           switch (this.currentLvl) {
             case this.lvl0:
               this.currentLvl = this.lvl1;
@@ -164,7 +158,7 @@ class Pendu {
             this.Pendu_gameMessage.edit(emb);
           }
 
-        } else if ((!this.mots.includes(msg.content)) && this.lettersaid.includes(msg.content)) {
+        }else if (this.lettersaid.includes(msg.content)) {
           var emb = new discord.RichEmbed();
           emb.addField("Pendu", this.devinemot + "\n\n" + this.currentLvl + "\n\n" + this.lettersaid + "\n\n " + "You already said this letter ! : " + letter, false);
           this.Pendu_gameMessage.edit(emb);
